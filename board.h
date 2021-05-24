14873ee557244b253feb452b024c0646efe6c78b
Neil Hurley <neil.j.hurley@gmail.com>
2021-05-24 21:15:27 +0100

    Initial commit

---
+++ board.h
@@ -0,0 +1,11 @@
+//
+// Created by mario on 23/04/2021.
+//
+
+#ifndef UNTITLED2_FILE_H
+#define UNTITLED2_FILE_H
+
+#endif //UNTITLED2_FILE_H
+
+void initialise_board(char board[8][8],char player1name[10],char player2name[10]);
+void display_board(char board[8][8],char player1name[10],char player2name[10],int player1score, int player2score);
